4a781501056ac54ad520ec58431510676b5a7e8e
Vanderlei Campos <38666926+Vander27@users.noreply.github.com>
2019-02-18 14:10:20 -0300

    Commit
    Localização endereço por CEP

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Informe o CEP: ");
+            string cep = Console.ReadLine();
+
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/"  + cep + "/json/");
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+
+
+                Console.WriteLine(responseString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
